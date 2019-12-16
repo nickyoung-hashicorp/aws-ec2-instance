89e55f530240cc2a55a8aaaa50edf526bcdd79e7
nickyoung-hashicorp <nyoung@hashicorp.com>
2019-12-16 13:49:23 -0800

    commented EOF

--- main.tf
+++ main.tf
@@ -3,20 +3,20 @@ terraform {
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  user_data     = <<EOF
-      #!/bin/bash
-      yum install httpd -y
-      echo "<h1> Deployed by Terraform Cloud / Workspaces / multicloud </h1>" > /var/www/index.html
-      service httpd start
-      chkconfig httpd on
-      EOF
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  # user_data     = <<EOF
+  #     #!/bin/bash
+  #     yum install httpd -y
+  #     echo "<h1> Deployed by Terraform Cloud / Workspaces / multicloud </h1>" > /var/www/index.html
+  #     service httpd start
+  #     chkconfig httpd on
+  #     EOF
   tags = {
-    Name = "${var.name}"
+    Name = var.name
   }
 }
