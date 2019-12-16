9d250dcdb5fa1171b6a88af0213fe08008a4bf99
nickyoung-hashicorp <nyoung@hashicorp.com>
2019-12-16 13:45:10 -0800

    changed EOF line

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  user_data     = << EOF
+  user_data     = <<EOF
       #!/bin/bash
       yum install httpd -y
       echo "<h1> Deployed by Terraform Cloud / Workspaces / multicloud </h1>" > /var/www/index.html
