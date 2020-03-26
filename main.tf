0b8754e96d8956518f9e0005bbdafc770811afe1
nickyoung-hashicorp <nyoung@hashicorp.com>
2020-03-26 08:51:53 -0700

    tags

--- main.tf
+++ main.tf
@@ -12,10 +12,6 @@ resource "aws_instance" "ubuntu" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
-<<<<<<< HEAD
     # Name = var.name
-=======
-  //  Name = var.name
->>>>>>> 2b8c1785757a05255ea4c4707ccdfaa146c1524a
   }
 }
