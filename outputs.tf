3680f27c587446e191ff4f1e9eab719dbb1096c6
Nick Young <57373279+nickyoung-hashicorp@users.noreply.github.com>
2020-10-09 11:35:50 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "public_dns" {
-  value = "Default URL - ${module.aws_instance.hashicat.public_dns}"
-}
+# output "public_dns" {
+#   value = "Default URL - ${module.aws_instance.hashicat.public_dns}"
+# }
