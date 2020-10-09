b2fff6a7b756486facfefbeab5f959aecc8306ab
Nick Young <57373279+nickyoung-hashicorp@users.noreply.github.com>
2020-10-09 11:51:55 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ data aws_ami "ubuntu" {
 
 module "aws_instance" {
   source  = "app.terraform.io/nickyoung-hashicorp/aws-instance/aws"
-  version = "1.0.1"
+  version = "1.0.2"
   # insert required variables here
   security_group_ids = aws_security_group.hashicat.id
 }
