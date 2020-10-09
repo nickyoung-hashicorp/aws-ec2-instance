13bdc193c7b67d3cf2c284c4720da2deccf96235
Nick Young <57373279+nickyoung-hashicorp@users.noreply.github.com>
2020-10-09 12:11:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 # hulu-sentinel-test
 
-module "hulu-test" {
-  source  = "app.terraform.io/nickyoung-hashicorp/hulu-test/aws"
-  version = "1.0.0"
+provider "aws" {
+  region = var.aws_region
 }
+
+resource "aws_instance" "ubuntu" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = "${var.name}-master"
+    owner = var.owner
+  }
+}
+
+# module "hulu-test" {
+#   source  = "app.terraform.io/nickyoung-hashicorp/hulu-test/aws"
+#   version = "1.0.0"
+# }
