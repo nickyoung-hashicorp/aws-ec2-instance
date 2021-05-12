64cefe5fbfa20242293b0c49436ec1ca07dc2109
Nick Young <57373279+nickyoung-hashicorp@users.noreply.github.com>
2021-05-12 11:44:07 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "ubuntu" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
-#     Name = "aws-ec2-instance_prod"
-#     owner = "nyoung@hashicorp.com"
+    Name = "aws-ec2-instance_prod"
+    owner = "nyoung@hashicorp.com"
 #     ttl = "-1"
   }
 }
